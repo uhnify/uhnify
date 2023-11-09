9eb0cdcc5be7ff037dc2ab3cb4752c9149b196e0
Yujin Chen <yujin31@hawaii.edu>
2023-11-08 16:32:25 -1000

    fix eslint error

--- app/imports/ui/pages/ClubDetail.jsx
+++ app/imports/ui/pages/ClubDetail.jsx
@@ -8,7 +8,9 @@ const ClubDetail = () => (
     <p>Welcome to UHnify, the dedicated hub for connecting passionate students with vibrant campus clubs and communities!</p>
 
     <h1>Our Mission</h1>
-    <p>We believe that the heart of every educational institution beats through its clubs and organizations. They are the crossroads where culture thrives, interests are pursued, and lifelong friendships are forged. Our mission is simple yet profound: to create an accessible, seamless, and lively platform that not only simplifies the search for the perfect club but also lights the path to an enriching student life full of engagement and excitement.</p>
+    <p>We believe that the heart of every educational institution beats through its clubs and organizations. They are the crossroads where culture thrives, interests are pursued, and lifelong friendships are forged.
+      Our mission is simple yet profound: to create an accessible, seamless, and lively platform that not only simplifies the search for the perfect club but also lights the path to an enriching student life full of engagement and excitement.
+    </p>
 
     <h1>What We Offer</h1>
     <p>not done</p>
