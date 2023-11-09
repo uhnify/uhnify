0a590bfc02d0def292bc34b5006978c499aa188d
bobbyir <110771308+bobbyir@users.noreply.github.com>
2023-11-08 17:46:59 -1000

    More navbar functionality

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
+import { Container, Image, Carousel } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <div className="d-flex justify-content-center py-2">
-      <Image src="images/ManoaCampus.png" alt="On the Beach" />
-    </div>
+  <Container className="w-50:">
+    <Carousel className="py-4">
+      <Carousel.Item>
+        <Image className="d-block w-100" src="images/ManoaCampus.png" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className="d-block w-100" src="images/ManoaCampus.png" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className="d-block w-100" src="images/ManoaCampus.png" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   </Container>
 );
 
