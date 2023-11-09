f09646466c8970324f1c5b7c0f952a39bae9cfa7
bobbyir <110771308+bobbyir@users.noreply.github.com>
2023-11-08 18:48:54 -1000

    landing page 30% completion

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,31 +3,37 @@ import { Container, Image, Carousel } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container className="w-50:">
-    <Carousel className="py-4">
-      <Carousel.Item>
-        <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
-        <Carousel.Caption>
-          <h3>Accounting Club</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  </Container>
+  <>
+    <h1 className="d-flex justify-content-center">Welcome to UHnify</h1>
+    <p className="d-flex justify-content-center"> Discover, Connect, and Join Clubs at UH with Ease</p>
+    <hr />
+
+    <Container className="w-50:">
+      <Carousel className="py-4">
+        <Carousel.Item>
+          <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
+          <Carousel.Caption>
+            <h3>Accounting Club</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image className="d-block w-100 h-custom" src="images/AccountingClub.png" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
+  </>
 );
 
 export default Landing;
