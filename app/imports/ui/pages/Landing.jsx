c0125570f8caac112bf24aae5e847dd707b65692
bobbyir <110771308+bobbyir@users.noreply.github.com>
2023-11-09 16:59:17 -1000

    landing update

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,7 +7,26 @@ const Landing = () => (
     <h1 className="d-flex justify-content-center">Welcome to UHnify</h1>
     <p className="d-flex justify-content-center"> Discover, Connect, and Join Clubs at UH with Ease</p>
     <hr />
+    <Container fluid>
+      <Row className="d-flex justify-content-center">
+        <Col sm={4} lg={4} className="text-center">
+          <h2 style={{ color: '#217eab' }}>Explore Clubs</h2>
+          <p className="lead">Browse through the various clubs UH has to offer.</p>
 
+        </Col>
+
+        <Col sm={4} lg={4} className="text-center">
+          <h2 style={{ color: '#217eab' }}>Explore Events</h2>
+          <p className="lead">Browse through the various events UH has to offer.</p>
+          {/* Insert events calendar or list here */}
+        </Col>
+        <Col sm={4} lg={4} className="text-center">
+          <h2 style={{ color: '#217eab' }}>Get Started</h2>
+          <p className="lead">Start your adventure</p>
+          {/* Insert events calendar or list here */}
+        </Col>
+      </Row>
+    </Container>
     <Container className="carousel-container">
       <Carousel className="py-4">
         <Carousel.Item>
@@ -33,22 +52,7 @@ const Landing = () => (
         </Carousel.Item>
       </Carousel>
     </Container>
-    <Container className="width-50">
-      <Row>
-        <Col>
-          <h2>Explore Clubs</h2>
-          <p className="lead">Browse through the various clubs UH has to offer.</p>
 
-        </Col>
-
-        <Col>
-          <h2>Upcoming Events</h2>
-          <p className="lead">Browse through the various clubs UH has to offer.</p>
-
-          {/* Insert events calendar or list here */}
-        </Col>
-      </Row>
-    </Container>
   </>
 );
 
