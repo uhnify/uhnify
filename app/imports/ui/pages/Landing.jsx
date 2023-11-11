1f4d1c3909d7fac9a97e73ddeb0e461097797e99
bobbyir <110771308+bobbyir@users.noreply.github.com>
2023-11-10 17:40:28 -1000

    push

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,13 +1,127 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
-
+import { useEffect } from 'react';
+import { Image, Container, Row, Col, Card } from 'react-bootstrap';
+import $ from 'jquery';
+import 'owl.carousel';
 /* A simple static component to render some text for the landing page. */
-const Landing = () => (
-  <>
-    <h1 className="d-flex justify-content-center">Welcome to UHnify</h1>
-    <Image fluid src="images/ManoaImage.png" />
-  </>
 
-);
-
+const Landing = () => {
+  useEffect(() => {
+    // Initialize the carousel with specific options
+    $('.owl-carousel').owlCarousel({
+      loop: false,
+      margin: 10,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      center: true,
+      navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  }, []);
+  return (
+    <Container fluid className="p-0">
+      <main>
+        <Container fluid className="py-5">
+          <div className="d-flex justify-content-center align-items-center">
+            <Image src="/images/DALLEUHnify.png" alt="Main campus image" />
+          </div>
+        </Container>
+        {/* Cards section */}
+        <section id="slider" className="pt-5">
+          <div className="container">
+            <h1 className="text-center"><b>Responsive Owl Carousel</b></h1>
+            <div className="slider">
+              <div className="owl-carousel">
+                <div className="slider-card">
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <Image src="/images/DALLEUHnify.png" alt="" />
+                  </div>
+                  <h5 className="mb-0 text-center"><b>HTML CSS3 Tutorials</b></h5>
+                  <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                </div>
+                <div className="slider-card">
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <Image src="/images/DALLEUHnify.png" alt="" />
+                  </div>
+                  <h5 className="mb-0 text-center"><b>Wordpress Tutorials</b></h5>
+                  <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                </div>
+                <div className="slider-card">
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <Image src="/images/DALLEUHnify.png" alt="" />
+                  </div>
+                  <h5 className="mb-0 text-center"><b>PHP MySQL Tutorials</b></h5>
+                  <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                </div>
+                <div className="slider-card">
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <Image src="/images/DALLEUHnify.png" alt="" />
+                  </div>
+                  );
+                  <h5 className="mb-0 text-center"><b>Javascript Tutorials</b></h5>
+                  <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                </div>
+                <div className="slider-card">
+                  <div className="d-flex justify-content-center align-items-center mb-4">
+                    <Image src="/images/DALLEUHnify.png" alt="" />
+                  </div>
+                  <h5 className="mb-0 text-center"><b>Bootstrap Tutorials</b></h5>
+                  <p className="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Container>
+            <Row>
+              <Col> {/* Added lg={3} for larger screens */}
+                <Card> {/* Added custom-card class for additional styling */}
+                  <Card.Img variant="top" src="images/DALLEUHnify.png" alt="Events" />
+                  <Card.Body className="card-content">
+                    <Card.Title>Events</Card.Title>
+                    <Card.Text>Join the community in exciting events around campus.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col> {/* Same changes as above */}
+                <Card>
+                  <Card.Img variant="top" src="images/DALLEUHnify.png" alt="Browse Clubs" />
+                  <Card.Body className="card-content">
+                    <Card.Title>Browse Clubs</Card.Title>
+                    <Card.Text>Discover clubs that match your interests and become a member.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col> {/* Same changes as above */}
+                <Card>
+                  <Card.Img variant="top" src="images/DALLEUHnify.png" alt="My Clubs" />
+                  <Card.Body className="card-content">
+                    <Card.Title>My Clubs</Card.Title>
+                    <Card.Text>View and manage your club memberships and activities.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </main>
+    </Container>
+  );
+};
 export default Landing;
