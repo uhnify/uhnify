ef7a4d90aafa8cb5715921fca56fbeaef11b8537
Juncell VA <JuncellV@hawaii.edu>
2023-11-14 19:58:39 -1000

    commit 1 (not done)
    gonna lose my brain cells, something wrong with styles.css, I need column (Login) to take 75% of the page and the "Sign up" column to take up 25% of the page.

--- app/imports/ui/pages/SignIn.jsx
+++ app/imports/ui/pages/SignIn.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Card, Container, Row, Col, Button } from 'react-bootstrap';
-import SimpleSchema from 'simpl-schema';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [redirect, setRedirect] = useState(false);
 
-  const signInSchema = new SimpleSchema({
-    email: String,
-    password: String,
-  });
-
-  const bridge = new SimpleSchema2Bridge(signInSchema);
-
-  const submitSignIn = (doc) => {
-    const { email, password } = doc;
+  const handleSubmit = (e) => {
+    e.preventDefault();
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         setError(err.reason);
@@ -33,29 +25,39 @@ const SignIn = () => {
   }
 
   return (
-    <Container fluid className="px-0">
-      <Row noGutters className="min-vh-100">
-        <Col md={6} className="d-flex align-items-center justify-content-center signin-section">
-          <Card className="auth-card">
-            <Card.Body>
-              <h1 className="login__content-title">Login to Your Account</h1>
-              <AutoForm schema={bridge} onSubmit={submitSignIn}>
-                <TextField name="email" placeholder="E-mail address" />
-                <TextField name="password" placeholder="Password" type="password" />
-                <ErrorsField />
-                <SubmitField className="login__form-btn" />
-              </AutoForm>
-              <div className="sign-up-link">
-                New Here? <a href="/signup">Sign Up</a>
-              </div>
-            </Card.Body>
-          </Card>
+    <Container fluid className="signin-container">
+      <Row className="min-vh-100">
+        <Col md={6} className="signin-form-section">
+          <Form onSubmit={handleSubmit}>
+            <h1>Login to Your Account</h1>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          {error && <div className="text-danger">{error}</div>}
         </Col>
-        <Col md={6} className="d-flex flex-column align-items-center justify-content-center bg-teal signup-section">
-          <div className="text-white text-center">
-            <h1 className="image-container-title">New Here?</h1>
-            <p className="image-container-text">Sign up and discover a great amount of new opportunities!</p>
-            <Button variant="light" href="/signup">Sign Up</Button>
+        <Col md={6} className="signup-call-to-action-section ">
+          <div className="signup-call-to-action">
+            <h1>New Here?</h1>
+            <p>Sign up and discover a great amount of new opportunities!</p>
+            <Button variant="outline-light" href="/signup">Sign Up</Button>
           </div>
         </Col>
       </Row>
