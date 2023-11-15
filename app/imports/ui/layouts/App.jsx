359cccc69c7d05b34dd25888fe39d0e9279e7369
jesse39972 <jesse39@hawaii.edu>
2023-11-15 11:56:32 -1000

    Added ClubFinder mockup

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -18,6 +18,7 @@ import ClubDetail from '../pages/ClubDetail';
 import ListClubs from '../pages/ListClubs';
 import ListEvents from '../pages/ListEvents';
 import ListClubAdmin from '../pages/ListClubAdmin';
+import ClubFinder from '../pages/ClubFinder';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 const App = () => {
@@ -39,6 +40,7 @@ const App = () => {
           <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
           <Route path="/upcoming-events" element={<ListEvents />} />
           <Route path="/my-clubs" element={<ProtectedRoute><ListClubs /></ProtectedRoute>} />
+          <Route path="/search-clubs" element={<ProtectedRoute><ClubFinder /></ProtectedRoute>} />
           <Route path="/clubdetail" element={<ProtectedRoute><ClubDetail /></ProtectedRoute>} />
           <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
           <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
