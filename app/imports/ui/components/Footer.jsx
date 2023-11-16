88a4b27f01f479b2f6d4ae2bc7efa670bbb10a62
Juncell VA <JuncellV@hawaii.edu>
2023-11-15 19:25:33 -1000

    commit 1
    Test footer, footer will be on every page at the end of the page.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
-
-/** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
+import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => (
-  <footer className="mt-auto py-3 dark-green-navbar">
+  <footer className="mt-auto py-3 bg-light">
     <Container>
-      <Col className="text-center text-white">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
-      </Col>
+      <Row>
+        <Col md={3}>
+          <h5>Contact Us</h5>
+          <address>
+            <p>EXAMPLE</p>
+            University of Hawaii at Manoa<br />
+            2500 Campus Road, Honolulu, HI 96822<br />
+            <a href="mailto:studenthelp@hawaii.edu">studenthelp@hawaii.edu</a><br />
+            (808) 956-0000
+          </address>
+        </Col>
+        <Col md={3}>
+          <h5>Quick Links</h5>
+          <ul className="list-unstyled">
+            <li>EXAMPLE</li>
+            <li><a href="/">Student Portal</a></li>
+            <li><a href="/">Club Directory</a></li>
+            <li><a href="/">Event Calendar</a></li>
+            {/* Other links */}
+          </ul>
+        </Col>
+        <Col md={3}>
+          <h5>Follow Us</h5>
+          <p>EXAMPLE</p>
+          {/* Social media icons */}
+        </Col>
+        <Col md={3}>
+          <h5>Support and Feedback</h5>
+          <ul className="list-unstyled">
+            <li>EXAMPLE</li>
+            <li><a href="/">Report an Issue</a></li>
+            <li><a href="/">Privacy Policy</a></li>
+            <li><a href="/">Terms of Use</a></li>
+            {/* Other legal links */}
+          </ul>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
