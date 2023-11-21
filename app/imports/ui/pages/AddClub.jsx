c2c38a3c9e50a5e9a4088993f087f5b86aa47bb6
Yujin Chen <yujin31@hawaii.edu>
2023-11-20 23:32:30 -1000

    Add Club

---
+++ app/imports/ui/pages/AddClub.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, LongTextField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Clubs } from '../../api/club/Club';
+
+// Create a schema to specify the structure of the data to appear in the form.
+const formSchema = new SimpleSchema({
+  name: String,
+  description: String,
+  location: String,
+  image: String,
+  meetingTime: String,
+  categories: {
+    type: String,
+    allowedValues: ['Academic', 'Social', 'Sports', 'Cultural', 'Other'],
+    defaultValue: 'Other',
+  },
+
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
+
+/* Renders the AddClub page for adding a document. */
+const AddClub = () => {
+
+  // On submit, insert the data.
+  const submit = (data, formRef) => {
+    const { name, description, image, meetingTime, location, categories } = data;
+    const owner = Meteor.user().username;
+    Clubs.collection.insert(
+      { name, description, image, owner, meetingTime, location, categories },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      },
+    );
+  };
+
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Add Club</h2></Col>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+            <Card>
+              <Card.Body>
+                <TextField name="name" />
+                <TextField name="image" />
+                <TextField name="location" />
+                <LongTextField name={'description'} />
+                <TextField name="meetingTime" />
+                <SelectField name="categories" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AddClub;
