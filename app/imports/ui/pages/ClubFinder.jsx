145a0ac0e7588e51318c8e48512dc19b509837ff
Yujin Chen <yujin31@hawaii.edu>
2023-11-22 11:54:23 -1000

    change the hard code card to component

--- app/imports/ui/pages/ClubFinder.jsx
+++ app/imports/ui/pages/ClubFinder.jsx
@@ -1,47 +1,48 @@
 import React from 'react';
 import { Row, Container, Col, Button, Card, Image, Form } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Clubs } from '../../api/club/Club';
+import ClubAddItem from '../components/ClubAddItem';
+import LoadingSpinner from '../components/LoadingSpinner';
 
-const ClubFinder = () => (
-  <Container className="py-3">
-    <Col>
+const ClubFinder = () => {
+  const { ready, clubs } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Club documents.
+    const subscription = Meteor.subscribe(Clubs.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Club documents
+    const clubItems = Clubs.collection.find({}).fetch();
+    return {
+      clubs: clubItems,
+      ready: rdy,
+    };
+  }, []);
+
+  return (ready ? (
+    <Container className="py-3">
+      <Col>
       <h2>Filter</h2>
       <Form>
         <Form.Check label="sports" />
         <Form.Check label="games" />
       </Form>
-    </Col>
-    <Row className="justify-content-center">
-      <Col className="text-center">
-        <h2>Clubs</h2>
       </Col>
-    </Row>
-    <Row xs={1} md={2} lg={3} className="g-4">
-      <Card className="h-100 card">
-        <Card.Header>
-          <Image width={100} src="https://upload.wikimedia.org/wikipedia/commons/f/f7/JaquesCookStaunton.jpg" />
-          <Card.Title>Chess Club</Card.Title>
-          <Card.Subtitle>8:00-9:00</Card.Subtitle>
-          <Card.Subtitle>Classroom</Card.Subtitle>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>We Play Chess</Card.Text>
-          <Button>Add to My Clubs</Button>
-        </Card.Body>
-      </Card>
-      <Card className="h-100 card">
-        <Card.Header>
-          <Image width={75} src="https://upload.wikimedia.org/wikipedia/commons/6/64/Football_signed_by_Gerald_R._Ford.jpg" />
-          <Card.Title>Football Club</Card.Title>
-          <Card.Subtitle>6:00-12:00pm</Card.Subtitle>
-          <Card.Subtitle>Football Field</Card.Subtitle>
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>We Play Football</Card.Text>
-          <Button>Add to My Clubs</Button>
-        </Card.Body>
-      </Card>
-    </Row>
-  </Container>
-);
+      <Row className="justify-content-center">
+        <Col>
+          <Col className="text-center">
+            <h2>My Clubs</h2>
+          </Col>
+          <Row xs={1} md={2} lg={3}>
+            {clubs.map((club) => (<Col key={club._id}><ClubAddItem club={club} /></Col>))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
 
 export default ClubFinder;
