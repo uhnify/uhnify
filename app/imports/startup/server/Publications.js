ebc2cd2fab1ff0aa17f3dcb695b27b01ed5d39c8
Christian Joudon <christian.joudon@gmail.com>
2023-11-22 20:27:53 -1000

    Commit Me

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -3,8 +3,13 @@ import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Clubs } from '../../api/club/Club';
 import { Events } from '../../api/events/Events';
-// User-level publication.
-// If logged in, then publish documents owned by this user. Otherwise, publish nothing.
+import { Profiles } from '../../api/profile/Profile'; // Import Profiles collection
+import { Interests } from '../../api/interest/Interest'; // Import Interests collection
+import { ClubInterests } from '../../api/clubInterest/ClubInterest'; // Import ClubInterests collection
+import { EventsInterests } from '../../api/eventInterest/EventInterest'; // Import EventsInterests collection
+import { ProfilesInterests } from '../../api/profileInterest/ProfileInterest'; // Import ProfilesInterests collection
+
+// User-level publications for Stuffs
 Meteor.publish(Stuffs.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
@@ -13,6 +18,15 @@ Meteor.publish(Stuffs.userPublicationName, function () {
   return this.ready();
 });
 
+// Admin-level publications for Stuffs
+Meteor.publish(Stuffs.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Stuffs.collection.find();
+  }
+  return this.ready();
+});
+
+// User-level publications for Clubs
 Meteor.publish(Clubs.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
@@ -21,15 +35,7 @@ Meteor.publish(Clubs.userPublicationName, function () {
   return this.ready();
 });
 
-// Admin-level publication.
-// If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
-Meteor.publish(Stuffs.adminPublicationName, function () {
-  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
-    return Stuffs.collection.find();
-  }
-  return this.ready();
-});
-
+// Admin-level publications for Clubs
 Meteor.publish(Clubs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Clubs.collection.find();
@@ -37,15 +43,7 @@ Meteor.publish(Clubs.adminPublicationName, function () {
   return this.ready();
 });
 
-// alanning:roles publication
-// Recommended code to publish roles for each user.
-Meteor.publish(null, function () {
-  if (this.userId) {
-    return Meteor.roleAssignment.find({ 'user._id': this.userId });
-  }
-  return this.ready();
-});
-
+// User-level publications for Events
 Meteor.publish(Events.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
@@ -54,9 +52,54 @@ Meteor.publish(Events.userPublicationName, function () {
   return this.ready();
 });
 
+// Admin-level publications for Events
 Meteor.publish(Events.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Events.collection.find();
+  }
+  return this.ready();
+});
+
+// User-level publications for Profiles
+Meteor.publish(Profiles.userPublicationName, function () {
+  if (this.userId) {
+    return Profiles.collection.find({ userId: this.userId });
+  }
+  return this.ready();
+});
+
+// Admin-level publications for Profiles
+Meteor.publish(Profiles.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Profiles.collection.find();
+  }
+  return this.ready();
+});
+
+// General publication for Interests
+Meteor.publish(Interests.publicationName, function () {
+  return Interests.collection.find();
+});
+
+// General publication for ClubInterests
+Meteor.publish(ClubInterests.publicationName, function () {
+  return ClubInterests.collection.find();
+});
+
+// General publication for EventsInterests
+Meteor.publish(EventsInterests.publicationName, function () {
+  return EventsInterests.collection.find();
+});
+
+// General publication for ProfilesInterests
+Meteor.publish(ProfilesInterests.publicationName, function () {
+  return ProfilesInterests.collection.find();
+});
+
+// Roles publication
+Meteor.publish(null, function () {
+  if (this.userId) {
+    return Meteor.roleAssignment.find({ 'user._id': this.userId });
   }
   return this.ready();
 });
