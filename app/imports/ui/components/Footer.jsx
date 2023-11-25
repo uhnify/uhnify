d00eed7f25c94f9a4825e8ea3cf3410cdaf7218f
Christian Joudon <christian.joudon@gmail.com>
2023-11-25 09:20:38 -1000

    Commit Me

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Col, Container, Row, Image, Button } from 'react-bootstrap';
-import {Instagram, Twitter, Facebook, Linkedin } from 'react-bootstrap-icons';
+import { Instagram, Twitter, Facebook, Linkedin } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="mt-auto py-3 dark-green-navbar">
+  <footer className="footer">
     <Container>
       <Row className="row-cols-1 row-cols-lg-3">
-        <Col>
+        <Col className="text-center text-lg-start">
           <Image src="images/UmifyLOGO.png" fluid alt="footerLogo" className="pb-3"/>
-          <Button variant="outline-primary" href="https://uhnify.github.io/" className="w-100">
+          <Button className="w-100 footer-button" href="https://uhnify.github.io/">
             UHnify Homepage
           </Button>
         </Col>
-        <Col className="text-white text-center text-lg-start">
+        <Col className="text-white text-center">
           <h5>Description</h5>
           <p>
             UHnify: The Ultimate Student Club Experience for UH Manoa - A platform revolutionizing student connections at the University of Hawaii at Manoa. Empowering students with interactive tools, UHnify fosters club engagement, personalized experiences, and streamlined club management. Powered by Meteor and MongoDB, UHnify offers a unified ecosystem, making university life more vibrant and connected.
@@ -23,7 +23,7 @@ const Footer = () => (
           <h5>Contact Us</h5>
           <p><strong>Email:</strong> Unify@hawaii.edu</p>
           <p><strong>Phone:</strong> (808) 956-8111</p>
-          <Button variant="outline-primary">
+          <Button className="footer-button" href="#">
             Unify chatbot
           </Button>
           <div className="social-icons pt-3">
