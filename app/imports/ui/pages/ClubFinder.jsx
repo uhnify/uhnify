5379cb0f8f075859f61ddaf38d46cdf07735b245
Yujin Chen <yujin31@hawaii.edu>
2023-11-25 21:23:41 -1000

    Update ClubFinder.jsx

--- app/imports/ui/pages/ClubFinder.jsx
+++ app/imports/ui/pages/ClubFinder.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Row, Container, Col, Button, Card, Image, Form } from 'react-bootstrap';
-import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Clubs } from '../../api/club/Club';
-import ClubAddItem from '../components/ClubAddItem';
+import Club from '../components/Club';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const ClubFinder = () => {
+
+  // ... code to fetch clubs ...
+  const addClubToProfile = (clubId) => {
+    console.log("working");
+    Meteor.call('profileClubs.add', clubId, (error) => {
+      if (error) {
+        console.error('Error adding club to profile:', error);
+      } else {
+        console.log('Club added to profile successfully');
+      }
+    });
+  };
+
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, clubs } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
@@ -21,23 +35,19 @@ const ClubFinder = () => {
       ready: rdy,
     };
   }, []);
-
   return (ready ? (
-    <Container className="py-3">
-      <Col>
-      <h2>Filter</h2>
-      <Form>
-        <Form.Check label="sports" />
-        <Form.Check label="games" />
-      </Form>
-      </Col>
+    <Container id="browse-clubs-page" className="py-3">
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
             <h2>My Clubs</h2>
           </Col>
-          <Row xs={1} md={2} lg={3}>
-            {clubs.map((club) => (<Col key={club._id}><ClubAddItem club={club} /></Col>))}
+          <Row xs={1} md={2} lg={4}>
+            {clubs.map((club) => (
+              <Col key={club._id}>
+                <Club club={club} onAddToProfile={() => addClubToProfile(club._id)} />
+              </Col>
+            ))}
           </Row>
         </Col>
       </Row>
