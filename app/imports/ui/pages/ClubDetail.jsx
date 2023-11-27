cade243cc7be34e3f15459ac1bb9fec051df75d0
Juncell VA <JuncellV@hawaii.edu>
2023-11-26 21:10:19 -1000

    commit 2 (club detail page)
    no styles, need to retouch.

--- app/imports/ui/pages/ClubDetail.jsx
+++ app/imports/ui/pages/ClubDetail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 
 const ClubDetail = () => (
-  <Container>
+  <Container className="club-detail-container">
 
     <h1>About Us</h1>
     <p>Welcome to UHnify, the dedicated hub for connecting passionate students with vibrant campus clubs and communities!</p>
@@ -13,10 +13,16 @@ const ClubDetail = () => (
     </p>
 
     <h1>What We Offer</h1>
-    <p>not done</p>
+    <p>Discovery Made Simple: Gone are the days of bulletin board browsing or missing out on fantastic opportunities because the word didn’t reach you in time. Our platform offers a smart, user-friendly interface that allows you to discover clubs that match your interests with just a few clicks.</p>
+    <p>Personalized Club Feeds: Customize your feed to follow the clubs you're part of or interested in. Stay updated with their latest news, events, and announcements without the fear of missing out.</p>
+    <p>Event Showcases: From cultural nights to academic seminars, from spirited sports gatherings to creative workshops, our platform showcases an array of upcoming events. Plan your calendar with ease and never miss an event that sparks your interest.</p>
+    <p>One-Stop Club Management: For club leaders, we provide a suite of tools to manage memberships, promote events, and engage with your audience all in one place. Drive up participation and enthusiasm with our intuitive club management features.</p>
+    <p>Community at Your Fingertips: With a focus on fostering a vibrant student community, we offer a space for interaction, discussion, and collaboration. Whether you're a club member or just someone interested in attending, connect with like-minded peers and make every club event a memorable one.</p>
 
     <h1>Join the Movement</h1>
-    <p>not done</p>
+    <p>At UHnify, your next adventure is just a click away. Dive into a world of clubs that cater to every interest under the sun, be it academic, artistic, technological, philanthropic, or just plain fun. Join us now and become a part of the pulsating energy of student life!
+
+      Embark on your journey with UHnify today because every student's story deserves to be filled with chapters of unforgettable experiences!</p>
 
   </Container>
 );
