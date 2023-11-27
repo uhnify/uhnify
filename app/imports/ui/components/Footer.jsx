f628a6c7b6c9d3ab18e2e0af660896df986390b5
Juncell VA <JuncellV@hawaii.edu>
2023-11-26 20:26:25 -1000

    commit 1 (footer)
    Need to fix margins, center the columns and push them towards the center.

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,51 +1,52 @@
 import React from 'react';
 import { Col, Container, Row, Image, Button } from 'react-bootstrap';
 import {Instagram, Twitter, Facebook, Linkedin } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
+/** I need to center columns and push them towards the center vertical line */
 const Footer = () => (
-  <footer className="mt-auto py-3 dark-green-navbar">
-    <Container>
-      <Row xs={1} md={1} lg={3}>
-      <Col >
-        <Row className="pb-3">
-          <Image className="pt-3" src="images/UmifyLOGO.png" width="100%" height="auto" alt="footerLogo"/>
-        </Row>
-        <Row>
-        <Button variant="outline-primary" id="homepagebutton" href="https://uhnify.github.io/" width="100" height="100">
-          UHnify Homepage
-        </Button>
-        </Row>
+    <footer className="mt-auto py-3 dark-green-navbar">
+      <Container fluid className="footer">
+        <Row className="justify-content-center">
+          <Col md={1} className="footer-column">
+            <h2>SOCIALS</h2>
+            <hr className="heading-underline" />
+            <a href="https://www.facebook.com/uhmanoa/" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <Facebook className="px-2" size={45}/>
+            </a>
+            <a href="https://www.instagram.com/uhmanoanews/?hl=en" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <Instagram className="px-2" size={45}/>
+            </a>
+            <a href="https://twitter.com/uhmanoa?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <Twitter className="px-2" size={45}/>
+            </a>
+            <a href="https://www.linkedin.com/school/uhmanoa/" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <Linkedin className="px-2" size={45}/>
+            </a>
+          </Col>
 
-      </Col>
-      <Col className="text-center text-white">
-        <Row className="text-center text-white">
-          <h5>Description</h5>
+          <Col md={1} className="footer-column">
+            <h2>ABOUT</h2>
+            <hr className="heading-underline" />
+            <div><Link to="https://uhnify.github.io/" className="footer-link">GITHUB HOMEPAGE</Link></div>
+            <div><Link to="/clubdetail" className="footer-link">ABOUT US</Link></div>
+            <h2 className="pt-4">HELP</h2>
+            <hr className="heading-underline" />
+            <a href="mailto:juncellv@hawaii.edu" className="footer-link">CONTACT</a>
+          </Col>
+
+          <Col md={1} className="d-none d-md-block vertical-line-container">
+            <div className="vertical-line"></div>
+          </Col>
+
+          <Col md={2} className="footer-column">
+            <p>  <strong>UHnify</strong>: The Ultimate Student Club Experience for UH Manoa - A platform revolutionizing student connections at the University of Hawaii at Manoa. Empowering students with interactive tools, UHnify fosters club engagement, personalized experiences, and streamlined club management. Powered by Meteor and MongoDB, UHnify offers a unified ecosystem, making university life more vibrant and connected.</p>
+            <p className="py-5"><strong>© 2023 by UHnify</strong></p>
+          </Col>
         </Row>
-        <Row>
-          <p>
-            UHnify: The Ultimate Student Club Experience for UH Manoa - A platform revolutionizing student connections at the University of Hawaii at Manoa. Empowering students with interactive tools, UHnify fosters club engagement, personalized experiences, and streamlined club management. Powered by Meteor and MongoDB, UHnify offers a unified ecosystem, making university life more vibrant and connected.
-          </p>
-        </Row>
-      </Col>
-      <Col className="text-center text-white">
-          <h5>Contact Us</h5>
-        <p><strong>Email:</strong> Unify@hawaii.edu</p>
-        <p><strong>Phone:</strong> (808) 956-8111</p>
-        <Button variant="outline-primary" id="chatbotbutton" >
-          Unify chatbot
-        </Button>
-        <Row className="text-center text-white pt-3">
-          <h5>Follow Us</h5>
-        </Row>
-            <Facebook className="px-2" size={50}/>
-            <Instagram className="px-2" size={50}/>
-            <Twitter className="px-2" size={50}/>
-            <Linkedin className="px-2" size={50}/>
-      </Col>
-      </Row>
-    </Container>
-  </footer>
+      </Container>
+    </footer>
 );
 
 export default Footer;
