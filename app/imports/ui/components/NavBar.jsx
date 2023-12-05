d3f74e6d38fd620ab515137cbe03825aa8cc349d
Christian Joudon <christian.joudon@gmail.com>
2023-12-05 10:00:50 -1000

    Made A Logo in Photoshop (so long) and Got Rid of Arrows next to Navbar Words

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
     <Navbar expand="lg" className="dark-green-navbar py-xl-5">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          UHnify
+          <img src="/images/UHnifyLogoz.png" alt="UHnify Logo" width="250" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
