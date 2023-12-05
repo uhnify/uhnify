c0415cc9e1f0706c1c8c14f38e940d8a186dc095
bobbyir <110771308+bobbyir@users.noreply.github.com>
2023-12-04 18:21:58 -1000

    Added ability to filter the clubs by categories

--- app/imports/ui/pages/ClubFinder.jsx
+++ app/imports/ui/pages/ClubFinder.jsx
@@ -1,54 +1,92 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Clubs } from '../../api/club/Club';
 import Club from '../components/Club';
 import LoadingSpinner from '../components/LoadingSpinner';
-
+import { Modal, Button } from 'react-bootstrap';
+import { CiFilter } from "react-icons/ci";
 const ClubFinder = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [showModal, setShowModal] = useState(false);
 
-  // ... code to fetch clubs ...
-  const addClubToProfile = (clubId) => {
-    Meteor.call('profileClubs.add', clubId, (error) => {
-      if (error) {
-        console.error('Error adding club to profile:', error);
-      } else {
-        console.log('Club added to profile successfully');
-      }
-    });
-  };
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  // Fetch clubs and set up subscription
   const { ready, clubs } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
-    // Get access to Club documents.
     const subscription = Meteor.subscribe(Clubs.userPublicationName);
-    // Determine if the subscription is ready
-    const rdy = subscription.ready();
-    // Get the Club documents
     const clubItems = Clubs.collection.find({}).fetch();
     return {
       clubs: clubItems,
-      ready: rdy,
+      ready: subscription.ready(),
     };
   }, []);
+
+  // Set unique categories based on fetched clubs
+  useEffect(() => {
+    const uniqueCategories = new Set();
+    clubs.forEach(club => {
+      club.categories.forEach(category => {
+        uniqueCategories.add(category);
+      });
+    });
+    setCategories([...uniqueCategories]);
+  }, [clubs]);
+
+
+  const handleCategoryChange = (event, category) => {
+    if (event.target.checked) {
+      setSelectedCategories([...selectedCategories, category]);
+    } else {
+      setSelectedCategories(selectedCategories.filter(c => c !== category));
+    }
+  };
+  const filteredClubs = selectedCategories.length > 0
+    ? clubs.filter(club => club.categories.some(category => selectedCategories.includes(category)))
+    : clubs;
+
+
   return (ready ? (
     <Container id="browse-clubs-page" className="py-3">
-      <Row className="justify-content-center">
-        <Col>
-          <Col className="text-center">
-            <h2>Club Finder</h2>
+      <Button variant="light" onClick={handleShowModal}>
+        <CiFilter /> Filter
+      </Button>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Filter Clubs by Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {categories.map(category => (
+            <div key={category}>
+              <input
+                type="checkbox"
+                id={category}
+                checked={selectedCategories.includes(category)}
+                onChange={(e) => handleCategoryChange(e, category)}
+              />
+              <label htmlFor={category}>{category}</label>
+            </div>
+          ))}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Render filtered clubs */}
+      <Row xs={1} md={2} lg={4}>
+        {filteredClubs.map(club => (
+          <Col key={club._id}>
+            <Club club={club} onAddToProfile={() => addClubToProfile(club._id)} />
           </Col>
-          <Row xs={1} md={2} lg={4}>
-            {clubs.map((club) => (
-              <Col key={club._id}>
-                <Club club={club} onAddToProfile={() => addClubToProfile(club._id)} />
-              </Col>
-            ))}
-          </Row>
-        </Col>
+        ))}
       </Row>
     </Container>
   ) : <LoadingSpinner />);
