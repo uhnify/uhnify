f450ba60927a2194b36d98983ae4278253cec574
Christian Joudon <christian.joudon@gmail.com>
2023-12-06 11:19:29 -1000

    Commit Me

--- app/server/main.js
+++ app/server/main.js
@@ -1,4 +1,13 @@
+import { Meteor } from 'meteor/meteor';
+import fs from 'fs';
 import '/imports/startup/server/Accounts';
 import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
 import '/imports/startup/both/Methods';
+
+Meteor.startup(() => {
+  const filePath = Assets.absoluteFilePath('defaultClubs.json');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const defaultClubs = JSON.parse(fileContents);
+  // Now you can use 'defaultClubs' as needed
+});
