58a265b5b22f72616937e4bcaca214e47f413cae
Juncell VA <JuncellV@hawaii.edu>
2023-12-09 20:40:29 -1000

    add profile but seems to be in a loading loop

--- app/imports/ui/pages/ListClubAdmin.jsx
+++ app/imports/ui/pages/ListClubAdmin.jsx
@@ -6,25 +6,32 @@ import { Clubs } from '../../api/club/Club';
 import ClubItemAdmin from '../components/ClubItemAdmin';
 import { Events } from '../../api/events/Events'; // Adjust the path as necessary
 import EventCardAdmin from '../components/EventsAdmin';
+import ProfileCard from '../components/Profiles';
+import { Profile } from '../../api/profile/Profile';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders a table containing all of the Club documents. Use <ClubItemAdmin> to render each row. */
 const ListClubAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { clubs, events, ready } = useTracker(() => {
+  const { clubs, events, profiles, ready } = useTracker(() => {
     // Get access to Club documents.
     const clubSubscription = Meteor.subscribe(Clubs.adminPublicationName);
     //
     const eventSubscription = Meteor.subscribe(Events.adminPublicationName);
+    //
+    const profileSubscription = Meteor.subscribe(Profile.adminPublicationName);
+
     // Determine if the subscription is ready
-    const rdy = clubSubscription.ready() && eventSubscription.ready();
+    const rdy = clubSubscription.ready() && eventSubscription.ready() && profileSubscription.ready();
     // Get the Club documents
     const itemClub = Clubs.collection.find({}).fetch();
     const itemEvent = Events.collection.find({}).fetch();
+    const itemProfile = Profile.collection.find({}).fetch();
 
     return {
       clubs: itemClub,
       events: itemEvent,
+      profile: itemProfile,
       ready: rdy,
     };
   }, []);
@@ -46,7 +53,17 @@ const ListClubAdmin = () => {
             <h2>All Events</h2>
           </Col>
           <Row xs={1} md={2} lg={4}>
-            {events.map((event) => (<Col key={event._id}><EventCardAdmin event={event} collection={Events.collection}/></Col>))}
+            {events.map((event) => (<Col key={event._id}><EventCardAdmin event={event} collection={Events.collection} /></Col>))}
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center py-5">
+        <Col>
+          <Col className="text-center">
+            <h2>All Profiles</h2>
+          </Col>
+          <Row xs={1} md={2} lg={4}>
+            {profiles.map((profile) => (<Col key={profile._id}><ProfileCard profile={profile} collection={Profile.collection} /></Col>))}
           </Row>
         </Col>
       </Row>
