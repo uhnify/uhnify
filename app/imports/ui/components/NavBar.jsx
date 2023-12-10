32605ef806edf08eccbfd5a3e50c3b338faf0d85
jesse39972 <jesse39@hawaii.edu>
2023-12-10 12:37:58 -1000

    added new profile image

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import { Meteor } from 'meteor/meteor';
 import { Container, Navbar, Nav, NavDropdown, Image } from 'react-bootstrap';
-import { Gear } from 'react-bootstrap-icons';
+import { Gear, PersonFill } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Profiles } from '../../api/profiles/Profiles';
 import LoadingSpinner from './LoadingSpinner'; // Import Bootstrap CSS
@@ -71,9 +71,9 @@ const NavBar = () => {
               title={
                 currentUser ? (
                   profile ? (
-                    <Image src={profile.picture || 'images/default-profile.png'} className="profilePicture" />
+                    <Image src={profile.picture || 'public/images/defaultprofilepic.png'} className="profilePicture" />
                   ) : (
-                    <Image src="images/default-profile.png" className="profilePicture" />
+                    <PersonFill />
                   )
                 ) : (
                   'Sign In'
